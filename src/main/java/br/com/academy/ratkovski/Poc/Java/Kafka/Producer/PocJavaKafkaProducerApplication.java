63cfa526291975bd152ddd80cfef185afcb6e989
Ratkovski <drezcry@hotmail.com>
2022-01-29 12:43:15 -0300

    chore: Codebase

---
+++ src/main/java/br/com/academy/ratkovski/Poc/Java/Kafka/Producer/PocJavaKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.Poc.Java.Kafka.Producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/academy/ratkovski/Poc/Java/Kafka/Producer/PocJavaKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.Poc.Java.Kafka.Producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
